cd3ebfce3f9688e2b85545712fdeceb8f6149e68
msrock10 <madihazafar58@gmail.com>
2025-01-22 18:00:43 +0500

    as

---
+++ main.py
@@ -0,0 +1,13 @@
+name="shees"
+age=11
+is_student=True
+weight=34.1
+
+print("name:",name)
+print(type(name ))
+print("age:",age)
+print(type(age))
+print("is_student:",is_student)
+print(type(is_student))
+print("weight:",weight)
+print(type(weight ))
